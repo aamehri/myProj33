8ca0f3dd4e552eeeace777bfc4a527e46f460696
aamehri <aamehri@loyola.edu>
2021-09-22 08:16:37 -0400

    com1

--- myProj33a.py
+++ myProj33a.py
@@ -5,3 +5,10 @@ c = 2
 print("a//b = ", a//b)
 print("a%b = ", a%b)
 print("b**c = ", b**c)
+if a==b :
+    print("passed")
+else :
+    if a <= b:
+        print("tada")
+    else :
+        print("Nope!")
